4208985258b0ddb5638bc3da0225746d1f98230f
Caio Fabio de Araujo <caio.araujo@linx.com.br>
2017-10-09 09:31:58 -0300

    #Fix

--- src/main/kotlin/com/caioaraujo/kotlindemo/entities/Character.kt
+++ src/main/kotlin/com/caioaraujo/kotlindemo/entities/Character.kt
@@ -6,13 +6,26 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class Character {
+class Character constructor() {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int = 0
     lateinit var name: String
     lateinit var skill: String
+
+    constructor(id: Int, name: String, skill: String) : this() {
+        this.id = id
+        this.name = name
+        this.skill = skill
+    }
+
+    constructor(name: String, skill: String) : this() {
+        this.name = name
+        this.skill = skill
+    }
+
     override fun toString(): String {
         return "Character(id=$id, name='$name', skill='$skill')"
     }
--- src/test/kotlin/com/caioaraujo/kotlindemo/CharacterTest.kt
+++ src/test/kotlin/com/caioaraujo/kotlindemo/CharacterTest.kt
@@ -2,6 +2,7 @@ package com.caioaraujo.kotlindemo
 
 import com.caioaraujo.kotlindemo.entities.Character
 import com.caioaraujo.kotlindemo.services.CharacterService
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -20,9 +21,24 @@ class CharacterTest {
         var char = Character()
         char.name = "Jhon Snow"
         char.skill = "Know Nothing"
-        this.service.save(char)
-
-
+        char = this.service.save(char)
+        Assert.assertFalse(char.id == 0)
     }
+
+    @Test
+    fun testListAll(){
+        getListCharacters().iterator().forEach { character -> this.service.save(character) }
+        Assert.assertTrue(this.service.listAll().size > 0)
+    }
+
+    fun getListCharacters(): Array<Character> {
+        var char1 = Character("John Snow", "Know Nothing")
+        var char2 = Character("Daenerys Targaryen", "Dracarius")
+        var char3 = Character("Aria Stark", "Vallar Morghulis")
+        val characterList = arrayOf(char1, char2, char3)
+        return characterList
+    }
+
+
 
 }
